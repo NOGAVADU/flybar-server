4d104ad85037049b89cc23fe69f8fbd1f2092853
Даниил Мосолов <dkuets@gmail.com>
2024-02-28 11:43:20 +0300

    change adminRouter

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -4,8 +4,10 @@ const adminController = require("../controllers/adminController");
 const authMiddleware = require("../middleware/authMiddleware");
 const checkAdmin = require('../middleware/checkAdmin')
 
-router.post("/create", checkAdmin(), adminController.create);
+router.post("/create", adminController.create);
 router.post("/login", adminController.login);
 router.get("/auth", authMiddleware, adminController.check);
 
 module.exports = router;
+
+// , checkAdmin()
