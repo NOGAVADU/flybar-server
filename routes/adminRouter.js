3aacc6309ea0470cae4e15690fd276ce8a0cc584
Даниил Мосолов <dkuets@gmail.com>
2024-02-28 16:02:59 +0300

    remove addAdmin route

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -4,7 +4,6 @@ const adminController = require("../controllers/adminController");
 const authMiddleware = require("../middleware/authMiddleware");
 const checkAdmin = require('../middleware/checkAdmin')
 
-router.post("/create", adminController.create);
 router.post("/login", adminController.login);
 router.get("/auth", authMiddleware, adminController.check);
 
