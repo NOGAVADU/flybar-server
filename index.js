06e5a145b778c58c9c84c301d28bb91fd20ce85d
Даниил Мосолов <dkuets@gmail.com>
2024-05-11 19:37:09 +0300

    add access control origin

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models");
 const cors = require("cors");
-const fileUpload  = require('express-fileupload')
+const fileUpload = require('express-fileupload')
 const router = require("./routes/index");
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path');
@@ -11,9 +11,13 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors({
-    origins: ["https://www.flybar.ru", "http://www.flybar.ru"]
-}));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Разрешить запросы со всех доменов
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Разрешить различные HTTP методы
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Разрешить различные заголовки
+    next();
+});
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
